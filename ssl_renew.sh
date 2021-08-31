0e36f0a6be0e7aaf7680be9dc983062a594d38e9
matiasop <movalle4@uc.cl>
2021-08-31 18:20:06 +0000

    ssl renew

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,6 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/ubuntu/app-to-iic2173
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP app
+#$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP app
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP app
 $DOCKER system prune -af
